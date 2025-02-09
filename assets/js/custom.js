a4c02a3cb6ee17b88dd4f8f14bff433c5fe9fe2a
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-02-09 09:15:10 -0800

    more updates
    - made footer automatically update copyright based on current year
    - converted index.html into a typical template page
    - added index2.html as a reference in case things break
    - put all scripts inside custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,3 +29,28 @@ navbarToggle.addEventListener("click", () => {
   }
 });
 
+
+
+
+//JS for project browser. I found it on CodePen back in 2021.
+//I can't seem to find it anymore...
+document.addEventListener("DOMContentLoaded", function() {
+  // get all tab pane divs within tab-header class
+  let tabHeader = document.getElementsByClassName("tab-header")[0]
+  if (!tabHeader) return; // exit script if tab-header class does not exist on page
+  let tabPanes = tabHeader.getElementsByTagName("div");
+
+  // loop through all tab panes and add click event
+  for(let i=0;i<tabPanes.length;i++){
+      tabPanes[i].addEventListener("click", function(){
+          // on click, remove current active header, make clicked header active
+          document.getElementsByClassName("tab-header")[0].getElementsByClassName("active-tab")[0].classList.remove("active-tab");
+          tabPanes[i].classList.add("active-tab");
+          // change tab indicator position based on index of pane clicked
+          document.getElementsByClassName("tab-indicator")[0].style.top = `calc(25px + ${i*50}px)`;
+          // swap active for tab content as well
+          document.getElementsByClassName("tab-content")[0].getElementsByClassName("active-tab")[0].classList.remove("active-tab");
+          document.getElementsByClassName("tab-content")[0].getElementsByTagName("div")[i*2].classList.add("active-tab");
+      })
+  }
+});
